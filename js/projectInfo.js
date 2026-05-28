f78119d1d5e0b00e22c716bcb3d3e2d9d4833391
Thols-Studio <40679920+Tholkappiyank@users.noreply.github.com>
2026-05-28 09:42:48 +0530

    Youtube details Updated

--- js/projectInfo.js
+++ js/projectInfo.js
@@ -26,7 +26,7 @@ const Projects = {
             SmallDescription: "PC, Story Telling, Unity 3D",
             LargeDescription: '<b>Story Telling</b> <br><br> Once it so happened that there was a beautiful contest between the sun and the wind. It was to see which of the two was stronger than the other.The wind was very proud of itself and its prowess. It said, “Even the mighty clouds bow before me. They know that I can drive them away.And the clouds can envelop you. So you are a small thing before me.” The sun said, “It is the time that will tell you who is stronger of the two.”Soon they saw a man who was wearing a coat. The sun said, “Let us see who makes the man to take off his coat.”The wind tried first. It blew harder and harder. But the man instead of removing his coat, wrapped it more and more. So, the wind failed.Now it was the turn of the sun to try its strength. It began to shine. It became hotter and hotter.Fiercer and fiercer. Ultimately, the man felt obliged to remove the coat. The wind lost and the sun won. <br><br> <b>Moral</b> <br> <br> A Smile can achieve what a brutal force cant. <br><br>• Sun And Wind is a Interactive Story Telling Application built on Unity3D for windows standalone platform. <br>• A 2 month project under Happy Mongo, Bangalore <br>• Programmed for interactive story telling ',
             DownloadLink: "",
-            EmbedVideoLinkID: "",
+            EmbedVideoLinkID: "Psvj9kgcXtE",
             NoOfProjectImages: 8,
             CarouselWidth: 100,
             ShowSlider: true
@@ -35,20 +35,20 @@ const Projects = {
             ID: "Platformer2D",
             Name: "Prototypes/Game Mimics",
             SmallDescription: "Hobby Project, Unity 3D, C#",
-            LargeDescription: '<b>Android Platform - Game </b> <br><br> • Recreation of the Mario Game to understand the Game mechanics and 2D level design features available on Unity3D for Android platform. <br>• A hobby project to understand how to move, jump, hit, shoot the enemy. ',
+            LargeDescription: '<b>Android Platform - Game </b> <br><br> • Reverse Engineering of Mario Game to understand the Game mechanics and 2D level design features (Tile System) available on Unity3D for Android platform. <br>• A hobby project to understand the mechanics - move, jump, hit, shoot the enemy. ',
             DownloadLink: "",
-            EmbedVideoLinkID: "x55Be6D1klg",
+            EmbedVideoLinkID: "gO-BlDvBPuQ",
             NoOfProjectImages: 9,
             CarouselWidth: 100,
             ShowSlider: true
         },	
 		JungleBoy: {
-			ID: "JungleBoy",
+	    ID: "JungleBoy",
             Name: "Prototypes/Game Mimics",
             SmallDescription: "College Project, XNA, C#, 2D",
             LargeDescription: '<b>PC Platform - Game </b> <br><br> • Recreation of the Rayman Origin Game to understand the Game mechanics and 2D level design features on XNA, C# for PC platform. <br>• A College project to understand how to move, jump. <br> • Menu System <br> • Terrain System  <br> • Collision Detection <br>  • Jumping <br>  • Points',
             DownloadLink: "",
-            EmbedVideoLinkID: "o4G4n7d9Ez4",
+            EmbedVideoLinkID: "dPW6sTDfoGg",
             NoOfProjectImages: 7,
             CarouselWidth: 97,
             ShowSlider: true
@@ -59,7 +59,7 @@ const Projects = {
             SmallDescription: "Unity Certification Project, Unity3D, C#",
             LargeDescription: '<b>Android Platform - Game </b> <br><br> • Roll a Ball Game to understand the Unity3D Software features <br>• A Unity3d Certification project <br>  • Unity User Interface <br> • Using GameObjects and Assets <br> • Managing Projects and Assets  <br> • Assembling the Game Level <br>  • Lighting in Games <br>  • Animating GameObjects in the Unity Editor <br>  • Scripting in Game Development <br>  • Implementing Navigation and Pathfinding <br>  • Creating Particle Systems<br>  • Adding Audio to Game Levels <br>  • Designing User Interfaces for Games<br>  • Building and Deploying the Game <br>  • Preparing for Mobile Deployment',
             DownloadLink: "",
-            EmbedVideoLinkID: "26fyEkfcreI",
+            EmbedVideoLinkID: "",
             NoOfProjectImages: 8,
             CarouselWidth: 100,
             ShowSlider: true
@@ -114,7 +114,7 @@ const Projects = {
             SmallDescription: "Unity3D, C#, Hyper Casual",
             LargeDescription: "<b>Placeholder</b> <br><br> Details coming soon.",
             DownloadLink: "",
-            EmbedVideoLinkID: "pLsSE9P8ZxQ",
+            EmbedVideoLinkID: "uCteOgpposU",
             NoOfProjectImages: 10,
             CarouselWidth: 100,
             ShowSlider: true
